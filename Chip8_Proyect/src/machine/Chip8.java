44ea192498d6fb974e48a280700d6ed424a6a1f8
David Cruz García <david.cruz.garciaa@gmail.com>
2020-09-08 12:49:57 +0200

    Added Load Rom function

--- Chip8_Proyect/src/machine/Chip8.java
+++ Chip8_Proyect/src/machine/Chip8.java
@@ -1,5 +1,9 @@
 package machine;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 /**
  *
  * @author David
@@ -18,6 +22,20 @@ public class Chip8 {
     }
     
     
-    
-    
+    /**
+     * Loads a binary rom 
+     * @param rom To load
+     * @throws java.io.IOException
+     */
+    public void loadRom(String rom) throws IOException{
+        File f = new File("ROMS/"+rom);
+        byte[] loadedRom = Files.readAllBytes(f.toPath());
+        
+        //Starts to load in 0x200 (where PC starts)
+        short addressToLoad = 0x200; 
+        for(byte b: loadedRom){
+            this.memory.set(addressToLoad, b);
+            addressToLoad += 0x1;
+        }
+    }     
 }
